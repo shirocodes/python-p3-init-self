c3982bd7306d475af047c74adba11d438b16175c
shirocodes <ishirapt@gmail.com>
2025-05-12 15:59:36 +0300

    Integrated self in instance methods.

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed ='Mutt'):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print('Woof!')
+
+    def adopt(self, owner_name):
+        self.owner = owner_name
+
+fido = Dog('Fido', 'Bosco')
+print(fido.name)
+print(fido.breed)
+
+fido.adopt('Sophie')
+print(fido.owner)
